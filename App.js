94a3e4726e9c7a19ab62babd8b5941b83c9f2bf5
Victor Cotrim <cotrim149@gmail.com>
2018-05-23 14:57:03 -0300

    Make App class like root, refactor App class to call root navigator and adding Home screen

--- App.js
+++ App.js
@@ -1,65 +1,17 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  ScrollView } from 'react-native';
 
-import Bananas from './components/Bananas';
-import Greetings from './components/Greetings';
-import Blink from './components/Blink';
-import LotsOfStyle from './components/LotsOfStyle';
+import {Text} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-import FixedDimensions from './components/FixedDimensionsBasics';
-import FlexDimensions from './components/FlexDimensionsBasics';
-import FlexDirection from './components/FlexDirection';
-import JustifyContentBasics from './components/JustifyContentBasics';
-import AlignItems from './components/AlignItems';
+import HomeScreen from './screens/HomeScreen';
 
-import PizzaTranslator from './components/PizzaTranslator';
-
-import ButtonBasics from './components/ButtonBasics';
-import Touchables from './components/Touchables';
-
-import ScrollPages from './components/ScrollPages';
-
-export default class HelloWorldApp extends Component {
+const RootStack = createStackNavigator({
+  App: {
+    screen: HomeScreen
+  },
+});
+export default class App extends Component {
   render() {
-    return (
-
-      <ScrollView style = {{marginTop: 20}}>
-
-        <Text>Touchables buttons</Text>
-        <Touchables/>
-
-        <Text>Basics of Button</Text>
-        <ButtonBasics/>
-
-        <Text>Pizza Translator</Text>
-        <PizzaTranslator/>
-
-        <Text>Flex: Align Items</Text>
-        <AlignItems/>
-
-        <Text>Flex: Justify content </Text>
-        <JustifyContentBasics/>
-
-        <Text>Flex: Direction</Text>
-        <FlexDirection/>
-
-        <Text>Flex lessons</Text>
-        <FlexDimensions/>
-        <FixedDimensions/>
-
-        <LotsOfStyle/>
-
-        <Blink text={'Blinking TEXT!'} />
-
-        <Greetings name='Cotrim'/>
-        <Greetings name='Jaina'/>
-
-        <Bananas></Bananas>
-        <Text>Hello world!</Text>
-      </ScrollView>
-    );
+    return <RootStack />;
   }
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+
+import {
+  Text,
+  View,
+  ScrollView } from 'react-native';
+
+import Bananas from '../components/Bananas';
+import Greetings from '../components/Greetings';
+import Blink from '../components/Blink';
+import LotsOfStyle from '../components/LotsOfStyle';
+
+import FixedDimensions from '../components/FixedDimensionsBasics';
+import FlexDimensions from '../components/FlexDimensionsBasics';
+import FlexDirection from '../components/FlexDirection';
+import JustifyContentBasics from '../components/JustifyContentBasics';
+import AlignItems from '../components/AlignItems';
+
+import PizzaTranslator from '../components/PizzaTranslator';
+
+import ButtonBasics from '../components/ButtonBasics';
+import Touchables from '../components/Touchables';
+
+import ScrollPages from '../components/ScrollPages';
+
+
+export default class HomeScreen extends Component {
+
+  render(){
+    return(
+      <ScrollView style = {{marginTop: 20}}>
+
+      <Text>Touchables buttons</Text>
+      <Touchables/>
+
+      <Text>Basics of Button</Text>
+      <ButtonBasics/>
+
+      <Text>Pizza Translator</Text>
+      <PizzaTranslator/>
+
+      <Text>Flex: Align Items</Text>
+      <AlignItems/>
+
+      <Text>Flex: Justify content </Text>
+      <JustifyContentBasics/>
+
+      <Text>Flex: Direction</Text>
+      <FlexDirection/>
+
+      <Text>Flex lessons</Text>
+      <FlexDimensions/>
+      <FixedDimensions/>
+
+      <LotsOfStyle/>
+
+      <Blink text={'Blinking TEXT!'} />
+
+      <Greetings name='Cotrim'/>
+      <Greetings name='Jaina'/>
+
+      <Bananas></Bananas>
+      <Text>Hello world!</Text>
+    </ScrollView>
+    );
+  }
+}
